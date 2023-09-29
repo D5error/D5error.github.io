07f2ee567a9c1a0730f4282405a0b299d98ed49d
D5rrr <1247054343@qq.com>
2023-09-29 16:19:43 +0800

    2023/9/29

--- task.cpp
+++ task.cpp
@@ -1,47 +1,60 @@
-/*³ÌĞò¹¦ÄÜ£ºÕâÀïÊµÑé¿É±ä²ÎÊıµÄº¯Êı,ÒÔ¼°¿É±ä²ÎÊıµÄºêµÄÌØĞÔ.
-*¿É±ä²ÎÊıº¯Êıvoid my_sum(int count, ...);
-*Õâ¸öº¯ÊıµÄ¹¦ÄÜÊÇ¼ÆËã¶à¸öÕûÊıµÄºÍ¡£
-*ÆäÖĞcountÊÇ½«ÒªÇóºÍµÄÕûÊıµÄÊıÄ¿¡£
-*ÆäËüµÄ²ÎÊıÊÇ¿É±äµÄ,ÆäÖĞµÚÒ»¸ö²ÎÊıÊÇchar*µÄ²ÎÊı£¬ÓÃÓÚÌáÊ¾¡£
-*ºóÃæµÄ²ÎÊı·Ö±ğÊÇ´ıÇóºÍµÄÕûÊı£¬Ò»¹²count¸ö¡£
-*/
-
-#include <stdarg.h>
-#include <stdio.h>
-
-void my_sum(int count, ...);
-
-int main(int argc, char *argv[])
+#include<iostream>
+#include<cmath>
+using namespace std;
+#define kType long long
+kType func(int m,int n)
 {
-        int count = 5;
-        printf("compute sum of %d numbers.\n",count);
-        my_sum(count,"the sum of numbers is:", 1, 2, 3, 4, 5);
-        return 0;
+    if(m==0)
+    {
+        return 1;
+    }
+    kType k=0;
+    for(int i=1;i<=n;i++)
+    {
+        k+=func(m-1,i);
+    }
+    return k;
 }
-
-void my_sum(int count, ...)
+kType ans[100];
+kType factorial(int n)
 {
-        //½«ÒªÓÃÀ´´æ·ÅĞèÒªµÄÄ³¸ö¿É±ä²ÎÊıµÄÖ¸ÕëµÄĞÅÏ¢
-        va_list ap;
-        char *prompt;
-        int sum = 0;
-
-        //¿ªÊ¼µÄ³õÊ¼»¯£¬ÆäÖĞapº¬ÓĞÖ¸Ïò¿É±ä²ÎÊıµÄÖ¸ÕëµÄĞÅÏ¢£¬countÊÇµ±Ç°º¯ÊıÖĞ×îºóÒ»¸ö·Ç¿É±äµÄ²ÎÊı(ÕâÑù²ÅÄÜ¶¨Î»).
-        va_start(ap, count);
-
-        //»ñÈ¡²¢·µ»ØÏÂÒ»¸ö¿É±ä²ÎÊıµÄÖµ£¬µÚÒ»¸ö²ÎÊıÊÇap²»ÓÃËµÁË£¬µÚ¶ş¸ö²ÎÊıÊÇÒª»ñÈ¡µÄ²ÎÊıµÄÀàĞÍ¡£
-        //¸ù¾İÎÄµµ£¬Èç¹ûÀàĞÍÖ¸¶¨´íÎóÁË£¬»òÕßÃ»ÓĞÏÂÒ»¸ö¿É±ä²ÎÊıÁË£¬ÄÇÃ´·µ»ØµÄ½á¹ûÊÇËæ»úµÄ¡£
-        prompt = va_arg(ap, char*);
-        printf("%s\n", prompt);
-
-        int i;
-        for(i = 0; i < count; ++i)
+    if(n==1)
+    {
+        ans[1]=1;
+    }
+    else 
+    {
+        ans[n]=ans[n-1]*n;
+    }
+    return ans[n];
+}
+kType C(int m,int n)
+{
+    //return pow(n,m);
+    return factorial(m+n-1)/factorial(m)/factorial(n-1);
+}
+int main()
+{
+    kType k=0;
+    int n,m;
+    cout<<"è¯·è¾“å…¥må’Œnï¼š"<<endl;
+    cin>>m>>n;
+    for(int x=1;x<=m;x++)
+    {
+        for(int i=1;i<=n;i++)
         {
-                sum += va_arg(ap, int);
+            printf("%5ld ",func(x,i));
         }
-
-        //Ê¹ÓÃÍê¿É±ä²ÎÊıÖ®ºóÒªÓÃÕâ¸öÀ´ÊÍ·Å×ÊÔ´
-        va_end(ap);
-        printf("%d\n",sum);
-
+        cout<<endl<<endl;
+    }
+    cout<<endl;
+    for(int x=1;x<=m;x++)
+    {
+        for(int i=1;i<=n;i++)
+        {
+            printf("%5ld ",C(x,i));
+        }
+        cout<<endl<<endl;
+    }
+    return 0;
 }
