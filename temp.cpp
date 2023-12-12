2a7274132c4a760ed568dce90f0326167be27277
D5rrr <1247054343@qq.com>
2023-12-12 10:45:15 +0800

    2023/12/12

--- temp.cpp
+++ temp.cpp
@@ -1,32 +1,38 @@
-#include<iostream>
-using namespace std;
+# include <iostream>
+# define MAX 1000
+int n; // 共有n个数，即1, 2, ..., n
+int ret[MAX]; // 存储排列结果
+bool used[MAX]; // 记录数是否被使用
+
+void dfs(int depth) // 参数u表示当前已访问的深度，即ret[]已保存的数字数量
+{
+    if(depth == n)  // dfs的层数等于最大深度，得出排列结果
+    {
+        for(int i = 0; i < n; i++)
+        {
+            printf("%d ", ret[i]);
+        }
+        printf("\n");
+        return ;
+    }
+
+    // 深度优先遍历
+    for(int i = 1; i <= n; i++)
+    {
+        if(!used[i])
+        {
+            ret[depth] = i;
+            used[i] = true;
+            dfs(depth + 1);
+            used[i] = false;
+        }
+    }
+}
+
+
 int main()
 {
-    int *a=new int[10];
-    for(int i=0;i<10;i++)
-    {
-        a[i]=i*100;
-    }
-    for(int i=0;i<10;i++)
-    {
-        cout<<a[i]<<endl;
-    }
-    cout<<endl;
-    delete []a;
-
-    
-    for(int i=0;i<10;i++)
-    {
-        cout<<a[i]<<endl;
-    } 
-    
-    cout<<endl;
-    delete []a;
-
-    
-    for(int i=0;i<10;i++)
-    {
-        cout<<a[i]<<endl;
-    } 
+    n = 4;
+    dfs(0);
     return 0;
 }
